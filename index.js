ce776c323aa7216814f8e0b75db601f9791d1d2a
Lanna Brenda <lannabrnd@gmail.com>
2023-11-27 21:49:27 -0300

    adicionando eventos a área de dúvidas

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 let elementosDuvida = document.querySelectorAll('.duvida')
 elementosDuvida.forEach(function(duvida){
     duvida.addEventListener('click', function(){
-        duvida.classList.add('fundo-verde')
+        duvida.classList.toggle("ativa")
     })
 })
-/*for each - executa para cada elemento
--> uma função pode ser declarada como
-function nome(argumentos){
 
-}
-*/
+
